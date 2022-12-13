137c70937a53d3496d3ccd07f23f95a61426472f
reidpat <61036489+reidpat@users.noreply.github.com>
2022-12-13 11:26:18 -0800

    Remaking the didabot scene

---
+++ Assets/Scripts/DidabotController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class DidabotController : MonoBehaviour
+{
+
+    
+    float speed = 25f;
+    float turnSpeed = 200f;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float frontRightDist = Raycast(45);
+        float frontLeftDist = Raycast(-45);
+        float backRightDist = Raycast(90);
+        float backLeftDist = Raycast(-90);
+
+
+
+        float frontRightPower = frontRightDist.Map(0,10,0.01f,1) / 1;
+        float frontLeftPower = frontLeftDist.Map(0,10,0.01f,1) / 1;
+        float backRightPower = backRightDist.Map(0,10,0.01f,1) / 1;
+        float backLeftPower = backLeftDist.Map(0,10,0.01f,1) / 1;
+        // throttlePower = frontDist.Map(0, 15, -0.3f, 1);
+
+        HandleMovement((frontRightPower + backRightPower) - (frontLeftPower + backLeftPower));
+    }
+
+
+    float Raycast(float yAngleOffset){
+
+        var direction = Quaternion.Euler(0,yAngleOffset,0) * transform.forward;
+        var position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(position, direction, out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(position, direction * hit.distance, Color.yellow);
+            return hit.distance;
+        }
+        else
+        {
+            Debug.DrawRay(position, direction * 50, Color.red);
+            return 1000f;
+        }
+    }
+
+
+    protected virtual void HandleMovement(float rotation){
+        float boundRotation = Mathf.Clamp(rotation, -1, 1);
+        //Movement
+        Vector3 wantedPosition = transform.position + (transform.forward * speed * Time.deltaTime);
+        rb.MovePosition(wantedPosition);
+
+        //Rotate
+        Quaternion wantedRotation = transform.rotation * Quaternion.Euler(Vector3.up * (boundRotation * turnSpeed * Time.deltaTime));
+        rb.MoveRotation(wantedRotation);
+    }
+}
